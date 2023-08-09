aab274d40ea68ab4922a0ec857f081828adadba9
annegog <gogoulaanna@gmail.com>
2023-08-09 23:15:12 +0300

    add a new place - interface

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import Perks from "../Perks";
 
 export default function PlacesPage() {
   const { action } = useParams();
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState("");
+  const [description, setDescription] = useState("");
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+
+  function inputHeader(text) {
+    return <h2 className="text-xl mt-4">{text}</h2>;
+  }
+
+  function addPhotoByLink(){
+    
+  }
+
   return (
     <div>
       {action !== "new" && (
@@ -29,28 +50,102 @@ export default function PlacesPage() {
       {action == "new" && (
         <div className="mt-4 grow items-center flex justify-around">
           <form>
-            <h2 className="text-xl mt-4">Title</h2>
-            <p className="text-gray-600 test-sm">Title for your place</p>
+            {inputHeader("Title")}
             <input
               type="text"
+              value={title}
+              onChange={(ev) => setTitle(ev.target.value)}
               placeholder="title, for example: My exotic house"
             />
-            <h2 className="text-xl mt-4">Address</h2>
-            <input type="text" placeholder="address" />
-            <h2 className="text-xl mt-4">Photos</h2>
+            {inputHeader("Address")}
+            <input
+              type="text"
+              value={address}
+              onChange={(ev) => setAddress(ev.target.value)}
+              placeholder="address"
+            />
+            {inputHeader("Photos")}
             <div className="flex gap-2">
-              <input type="text" placeholder={"Add using a link ... jpg"} />
-              <button className="add text-center ">
-                Add Photo
-              </button>
+              <input
+                type="text"
+                value={photoLink}
+                onChange={(ev) => setPhotoLink(ev.target.value)}
+                placeholder={"Add using a link ... jpg"}
+              />
+              <button className="add text-center">Add Photo</button>
             </div>
             <div className="mt-2 grid grid-cols-3 lg:grid-cols-6 md:grid-cols-4">
-              <button className="flex justify-around border bg-transparent rounded-xl p-8 text-2xl text-gray-450 ">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
-              </svg>
-               Upload
+              <button className="flex justify-center border bg-transparent rounded-xl p-8 text-2xl text-gray-450 ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-7 h-7"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15"
+                  />
+                </svg>
+                Upload
               </button>
+            </div>
+            {inputHeader("Description")}
+            <textarea
+              value={description}
+              onChange={(ev) => setDescription(ev.target.value)}
+              placeholder="Give a description about your place"
+            />
+            {inputHeader("Perks")}
+            <p className="text-gray-600 test-sm">
+              Select all the perks about your place
+            </p>
+            <div className="mt-2 grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+              <Perks selected={perks} onChange={setPerks} />
+            </div>
+            {inputHeader("Extra informations")}
+            <div className="mt-2 grid gap-2 sm:grid-cols-3">
+              <div>
+                <h3 className="mb-2">Check in time</h3>
+                <input
+                  type="text"
+                  value={checkIn}
+                  onChange={(ev) => setCheckIn(ev.target.value)}
+                  placeholder="13:00"
+                />
+              </div>
+              <div>
+                <h3 className="mb-2">Check out time</h3>
+                <input
+                  type="text"
+                  value={checkOut}
+                  onChange={(ev) => setCheckOut(ev.target.value)}
+                  placeholder="11:30"
+                />
+              </div>
+              <div>
+                <h3 className="mb-2">Maximum guests number</h3>
+                <input
+                  type="number"
+                  value={maxGuests}
+                  onChange={(ev) => setMaxGuests(ev.target.value)}
+                  placeholder="6"
+                />
+              </div>
+            </div>
+            <p className="text-gray-600 mt-2 test-sm">
+              Anything else you need to add
+            </p>
+            <textarea
+              value={extraInfo}
+              onChange={(ev) => setExtraInfo(ev.target.value)}
+              placeholder="The house rules, ect"
+            />
+            <div>
+              <button className="primary my-4">Save</button>
             </div>
           </form>
         </div>
